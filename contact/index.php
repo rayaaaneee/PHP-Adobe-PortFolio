ac709a4f4c2da0b9d4fdab9119f3fc1fa8784fc9
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2022-12-06 15:01:30 +0100

    Problème réglé je vais pleurer

--- contact/index.php
+++ contact/index.php
@@ -42,7 +42,7 @@
    <link sizes="180x180" href="../logos/favicon1.png" rel="icon" type="image/png">
    <title>Portfolio</title>
    <!-- FICHIERS PHP -->
-   <?php require_once "connect.php"; ?>
+   <?php require_once "../models/m_contact.php"; ?>
 </head>
 <body> 
    <header>

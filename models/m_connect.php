6f79cd69c600d83466f524aed27250bbaac23a91
rayanelecrackdecettegeneration <92400806+rayanemrln@users.noreply.github.com>
2023-01-28 22:01:12 +0100

    ut8 aws

--- models/m_connect.php
+++ models/m_connect.php
@@ -31,7 +31,14 @@ function getConnection(){
         $user = "root";
         $pass = "root";
         $port = "8889";
-        $db = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $pass);
+        $charset = "utf8mb4";
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $db = new PDO($dsn, $user, $pass, $options);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (Exception $e) {

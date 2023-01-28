27647ca25b3619e20efba2be3bc7e18e761ce19a
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-28 21:58:56 +0100

    changing charset

--- models/m_connect.php
+++ models/m_connect.php
@@ -6,7 +6,15 @@ function getConnection(){
         $dbname = "portfolio";
         $user = "root";
         $pass = "Opqdkjqo64$";
-        $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+        $port = "3306";
+        $charset = "utf8mb4";
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $db = new PDO($dsn, $user, $pass, $options);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (Exception $e) {

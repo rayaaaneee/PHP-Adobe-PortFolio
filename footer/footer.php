9685aa7ffdf654ae09a223aa41f61541b09140ae
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-29 02:00:15 +0100

    Fix footer

--- footer/footer.php
+++ footer/footer.php
@@ -4,9 +4,9 @@
         <p>2022, Copyright © - Rayane Merlin</p> 
     </div> 
     <div id="footer2"> 
-        <a href="https://github.com/rayaaaneee" id="footergithubimg" target="_blank"><img class="footerimgs" src="./footer/footer-logos/<?= $theme->getImagePath("github"); ?>.png"></a> 
-        <a href="https://www.linkedin.com/in/rayanemerlin/"id="footerlinkedinimg" target="_blank"><img class="footerimgs" src="./footer/footer-logos/<?= $theme->getImagePath("linkedin"); ?>.png"></a> 
-        <a href="mailto:rayane.merlin8@gmail.com" id="footermailimg" target="_blank"><img class="footerimgs" src="./footer/footer-logos/<?= $theme->getImagePath("mail"); ?>.png"></a> 
-        <a href="tel:+33768283277" id="footerphoneimg" target="_blank"><img class="footerimgs" src="./footer/footer-logos/<?= $theme->getImagePath("phone"); ?>.png"></a> 
+        <a href="https://github.com/rayaaaneee" id="footergithubimg" class="footer-github-img <?= $theme->getClass("footer-github-img"); ?>"target="_blank"></a> 
+        <a href="https://www.linkedin.com/in/rayanemerlin/" id="footerlinkedinimg" target="_blank" class=" footer-linkedin-img <?= $theme->getClass("footer-linkedin-img"); ?>"></a> 
+        <a href="mailto:rayane.merlin8@gmail.com" id="footermailimg" target="_blank" class="footer-mail-img <?= $theme->getClass("footer-mail-img"); ?>"></a> 
+        <a href="tel:+33768283277" id="footerphoneimg" target="_blank" class="footer-phone-img <?= $theme->getClass("footer-phone-img"); ?>"></a> 
     </div> 
 </footer> 

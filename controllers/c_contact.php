cc17c5913fbb2b7a0f079cbff7a8aa6acca0ef00
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-01-29 11:00:42 +0100

    Add time contact page

--- controllers/c_contact.php
+++ controllers/c_contact.php
@@ -13,14 +13,19 @@ if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
 // On récupère les données du formulaire et on les stocke dans la base de données
 $SucceedSend = false;
 if($wasSet){
+
     // Données formulaire
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
+    $date = date("Y-m-d");
+    // Ajouter 1 heure à l'heure actuelle
+    $time = date("H:i:s", strtotime($time . "+1 hours"));
+
     // BD
     $db = getConnection();
-    $stmt = getStatementContact($db, $name, $email, $message, date("Y-m-d H:i:s"));
-    // Insertion
+    $stmt = getStatementContact($db, $name, $email, $message, $date, $time);
+    
     try{
        $stmt->execute();
        // On affiche le message de confirmation
--- models/m_contact.php
+++ models/m_contact.php
@@ -1,11 +1,12 @@
 <?php
 
-function getStatementContact ($db, $name, $email, $message, $date) {
-    $sql = "INSERT INTO contacts (name, email, message, date) VALUES (:name, :email, :message, :date)";
+function getStatementContact ($db, $name, $email, $message, $date, $time) {
+    $sql = "INSERT INTO contacts (name, email, message, date, time) VALUES (:name, :email, :message, :date, :time)";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':name', $name);
     $stmt->bindParam(':email', $email);
     $stmt->bindParam(':message', $message);
     $stmt->bindParam(':date', $date);
+    $stmt->bindParam(':time', $time);
     return $stmt;
 }

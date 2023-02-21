e82e0e2c63fdd7f34c92bcfba81d5ee5b8c07428
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-02-21 19:35:05 +0100

    new config file

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Site
-define('PATH_ASSETS', './asset/');
+define('PATH_ASSETS', 'asset/');
 define('PATH_CSS', PATH_ASSETS . 'css/');
 define('PATH_SCRIPTS', PATH_ASSETS . 'js/');
 define('PATH_IMAGES', PATH_ASSETS . 'img/');
@@ -10,9 +10,9 @@ define('PATH_FONTS', PATH_ASSETS . 'fonts/');
 define('PATH_FILES', PATH_ASSETS . 'files/');
 
 // Path
-define('PATH_VIEWS', './view/v_');
-define('PATH_MODELS', './model/');
-define('PATH_CONTROLLERS', './controller/c_');
+define('PATH_VIEWS', 'view/v_');
+define('PATH_MODELS', 'model/');
+define('PATH_CONTROLLERS', 'controller/c_');
 define('PATH_DATABASE', PATH_MODELS . 'database/');
 define('PATH_PRESENTERS', PATH_MODELS . 'presenter/');
 define('PATH_DAO', PATH_DATABASE . 'dao/');
@@ -39,6 +39,6 @@ function defineConnection()
 
     define('DB_USR', 'root');
     define('DB_PWD', 'Opqdkjqo64$');
-}
+} 
 
 defineConnection();
